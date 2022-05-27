edfc5cd992d01f8c2b006170c0d86ced7e74bf26
Babatunde-Ben <babatundeben110.com>
2022-05-28 00:02:27 +0100

    initial commit

---
+++ src/pages/landing-page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <p className="test-header">This is my landing page</p>
+    </div>
+  );
+};
+
+export default LandingPage;
