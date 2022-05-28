804bec1f53ee017d719d55a679d1e4028a603fc3
Babatunde-Ben <babatundeben110.com>
2022-05-28 15:24:25 +0100

    routing fix

--- src/App.js
+++ src/App.js
@@ -11,12 +11,13 @@ function App() {
   const [userState, setUserState] = useState(false);
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <Navbar userState={userState} />
 
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/student" element={<h1>student</h1>} />
           <Route path="/dashboard" element={<h1>dashboard</h1>} />
           <Route path="/contact" element={<h1>contact</h1>} />
           <Route path="/luxury" element={<h1>luxury</h1>} />
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
+import { useNavigate } from "react-router-dom";
 import "../css/Login.css";
 import StudentLogin from "../images/student-login.svg";
 
 const Login = () => {
+  let navigate = useNavigate();
   const handleGoggleAuth = () => {
     alert("Signed In");
+    navigate("/student");
   };
   return (
     <section className="login-section">
@@ -14,7 +17,7 @@ const Login = () => {
         <h2 className="title">Student Login</h2>
         <p>Make sure you account is secure</p>
         <button className="sign-in-auth" onClick={handleGoggleAuth}>
-          Login with Google <FcGoogle />
+          Login with Google <FcGoogle className="google-icon" />
         </button>
       </div>
     </section>
