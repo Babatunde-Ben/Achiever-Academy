a4ed87012ebd8db88e057b7ca786674788061334
Babatunde-Ben <babatundeben110.com>
2022-05-29 03:05:52 +0100

    routing fix

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter basename="Achiever-Academ">
+      <BrowserRouter basename="/">
         <Navbar userState={userState} />
 
         <Routes>
