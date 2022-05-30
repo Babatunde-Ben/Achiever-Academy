e123544e591b42b83bdebcfacbc674b2dc69c207
Babatunde-Ben <babatundeben110.com>
2022-05-30 13:39:45 +0100

    added sidebar icons

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,18 +1,24 @@
 import React from "react";
 import "../css/Sidebar.css";
 import { NavLink } from "react-router-dom";
+import { MdOutlineSpaceDashboard } from "react-icons/md";
+import { GiBlackBook } from "react-icons/gi";
+import { CgProfile } from "react-icons/cg";
 
 const Sidebar = () => {
   return (
     <section className="sidebar-section">
       <li>
         <NavLink to="/student/dashboard">Dashboard</NavLink>
+        <MdOutlineSpaceDashboard className="sidebar-icons" />
       </li>
       <li>
         <NavLink to="/student/courses">Courses</NavLink>
+        <GiBlackBook className="sidebar-icons" />
       </li>
       <li>
         <NavLink to="/student/profile">Profile</NavLink>
+        <CgProfile className="sidebar-icons" />
       </li>
       <li>
         <NavLink to="/">
